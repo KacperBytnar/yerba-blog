f83d902ea8147dcf345d7f9859dcf7372d6df2e2
Mateista <kacper.bytnar19@gmail.com>
2023-03-05 05:54:36 +0100

    Fetching data from JSON server implemented

--- src/Home.js
+++ src/Home.js
@@ -4,30 +4,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     //let name = 'Mario';
-    const [blogs, setBlogs] = useState([
-        { title: "Health benefits of drinking yerba", body: "Lorem Ipsum...", author: "Mario Kumar", id: 1 },
-        { title: "Bitter Yerba Mate World", body: "Lorem Ipsum...", author: "Mateista", id: 2 },
-        { title: "How to Brew Yerba Mate: A Step-by-Step Guide - mateista", body: "Lorem Ipsum...", author: "Mateista", id: 3 },
+    const [blogs, setBlogs] = useState(null);
 
-    ]);
-
-    const [name, setName] = useState('Mateista');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id!== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(() => {
-        console.log('Use Effect ran');
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+            });
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-        <button onClick={() => setName('Mateista69')}>Change Name</button>
-        <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
